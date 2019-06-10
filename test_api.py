5a8c5df9b8e1eb554e1be70726f244a666061af3
ae <ae>
2019-06-09 18:04:57 -0700

    remove class

--- test_api.py
+++ test_api.py
@@ -6,22 +6,22 @@
 import api
 import pytest
 
-class Test_API:
+def test_obj_creation(self):
 
-    def test_obj_creation(self):
+    a = api.SMUD_API()
+    assert a is not None
 
-        a = api.SMUD_API()
-        assert a is not None
+@pytest.mark.slow
+def test_live_get_usage_by_day(self):
+    """ get LIVE usage for a day """
 
-    @pytest.mark.slow
-    def test_live_get_usage_by_day(self):
-        """ get LIVE usage for a day """
+    a = api.SMUD_API()
+    a.login(api.config['Auth']['username'], api.config['Auth']['password'])
+    date = (2019, 1, 1)
 
-        a = api.SMUD_API()
-        a.login(api.config['Auth']['username'], api.config['Auth']['password'])
-        date = (2019, 1, 1)
-
-        data = a.get("cost", "day", date)
-        assert type(data) is list
+    data = a.get("cost", "day", date)
+    assert type(data) is list
+    if isinstance(data, list):
+        assert len(data) > 0
 
 
